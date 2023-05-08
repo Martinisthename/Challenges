f7747869f8acc9f07000b04e1f9777d4719df852
martin siegert <oddnutshell@aol.com>
2023-05-08 16:54:08 +0200

    Add blackjack, croupier, and interface files

--- 01-Ruby/02-Flow-Array/04-Black-Jack/lib/croupier.rb
+++ 01-Ruby/02-Flow-Array/04-Black-Jack/lib/croupier.rb
@@ -1,9 +1,19 @@
 require_relative 'black_jack'
 
 def state_of_the_game(player_score, bank_score)
-  # TODO: return (not print!) a message containing the player's score and bank's score
+  # TODO: return (not print!) a message containing the player's score and bank's score.
+  "player score is: #{player_score} bank score is: #{bank_score}"
 end
 
 def end_game_message(player_score, bank_score)
   # TODO: return (not print!) a message telling if the user won or lost.
+  if player_score > 21 || player_score < bank_score
+    return "Lose"
+  elsif player_score == 21
+    return "Black Jack! You win"
+  elsif player_score > bank_score
+    return "You win"
+  elsif player_score == bank_score
+    return "Push. You get your money back"
+  end
 end
--- 01-Ruby/02-Flow-Array/04-Black-Jack/lib/interface.rb
+++ 01-Ruby/02-Flow-Array/04-Black-Jack/lib/interface.rb
@@ -2,3 +2,20 @@ require_relative "croupier"
 
 # TODO: make the user play from terminal in a while loop that will stop
 #       when the user will not be asking for  a new card
+
+player_score = 0
+bank_score = pick_bank_score
+
+while player_score < 22
+  puts "Card? 'y' to get a new card or 'n' to stop"
+  player_answer = gets.chomp
+
+  if player_answer == "y"
+    player_score += pick_player_card
+    puts state_of_the_game(player_score, bank_score)
+  elsif player_answer == "n"
+    puts state_of_the_game(player_score, bank_score)
+    break
+  end
+end
+puts end_game_message(player_score, bank_score)
